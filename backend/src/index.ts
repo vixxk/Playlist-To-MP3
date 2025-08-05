82b29befb6ff011fff8ba409293c0b35b32b0d94
Vivek-Anand727 <kamjorkismatwala@gmail.com>
2025-08-06 00:40:51 +0530

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import dotenv from "dotenv";
 import downloadRoute from "./routes/download.route";
@@ -6,6 +7,8 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({ origin: "*" }));
 
 app.get("/", (req,res) =>{
     res.send("Server is running");
